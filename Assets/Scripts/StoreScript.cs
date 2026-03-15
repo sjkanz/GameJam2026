1f82b35bd4dd5a5fe9955c67efcdf65ea47eb441
Isaac Lehman <isaaclehman21@gmail.com>
2026-03-15 11:32:18 -0400

    Merge upstream changes and resolve StoreScript conflict

--- Assets/Scripts/StoreScript.cs
+++ Assets/Scripts/StoreScript.cs
@@ -23,4 +23,61 @@ public class StoreScript : MonoBehaviour
         InventoryItem myItem = new InventoryItem("Apple", 1, 3);
         player.changeGroceryList(myItem);
     }
+
+    public void purchaseBadApple()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Apple", 0.75, 1);
+        player.changeGroceryList(myItem);
+    }
+
+    public void purchaseApplePie()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Apple Pie", 17, 2);
+        player.changeGroceryList(myItem);
+    }
+
+    public void purchaseGoodBanana()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Banana", 2, 5);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseMehBanana()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Banana", 1.75, 3);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseBadBanana()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Banana", 1.50, 3);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseChips()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Chips", 3, 2);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseRamen()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Ramen", 10, 2);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseRawFish()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Raw Fish", 15, 4);
+        player.changeGroceryList(myItem);
+    }
+    public void purchaseEggs()
+    {
+        print("purchasing...");
+        InventoryItem myItem = new InventoryItem("Eggs", 5, 3);
+        player.changeGroceryList(myItem);
+    }
 }
